0c48e39b073ea29d06916fefc67c7bfde2988066
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-10-24 15:51:29 -0300

    requisito 3 concluido

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInput = ({ target: { name, value } }) => {
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
   return (
     <div>
-      <input type="text" data-testid="email-input" />
-      <input type="text" data-testid="password-input" />
+      <input
+        type="text"
+        data-testid="email-input"
+        name="email"
+        value={ email }
+        onChange={ handleInput }
+      />
+      <input
+        type="text"
+        data-testid="password-input"
+        name="password"
+        value={ password }
+        onChange={ handleInput }
+      />
       <div>
         <button type="button" data-testid="login-submit-btn">Enter</button>
       </div>
