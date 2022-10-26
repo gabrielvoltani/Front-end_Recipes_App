471ce49620cb42e8f0406eecc91ced4092f82003
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-10-26 18:27:34 -0300

    requisito 25 concluido

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -10,14 +10,12 @@ function RecipeDetails(props) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
     const data = await response.json();
     setRecipe(data.meals[0]);
-    console.log(data);
   };
 
   const fetchDrink = async () => {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
     const data = await response.json();
     setRecipe(data.drinks[0]);
-    console.log(data);
   };
 
   useEffect(() => {
@@ -30,8 +28,41 @@ function RecipeDetails(props) {
     }
   }, []);
 
+  const isMeal = pathname.includes('meals');
+
   return (
-    <div>{recipe.strTags}</div>
+    <div>
+      <img
+        data-testid="recipe-photo"
+        src={ isMeal ? recipe.strMealThumb : recipe.strDrinkThumb }
+        alt={ isMeal ? recipe.strMeal : recipe.strDrink }
+      />
+      <h1 data-testid="recipe-title">{ isMeal ? recipe.strMeal : recipe.strDrink }</h1>
+      <h2 data-testid="recipe-category">
+        { isMeal ? recipe.strCategory : recipe.strAlcoholic }
+      </h2>
+      <h3>Ingredients</h3>
+      <ul>
+        {Object.keys(recipe).filter((key) => key.includes('strIngredient'))
+          .filter((key) => recipe[key] !== '' && recipe[key] !== null)
+          .map((key, index) => (
+            <li
+              data-testid={ `${index}-ingredient-name-and-measure` }
+              key={ key }
+            >
+              {recipe[key] && `${recipe[key]} - ${recipe[`strMeasure${index + 1}`]}`}
+            </li>
+          ))}
+      </ul>
+      <h3>Instructions</h3>
+      <p data-testid="instructions">{recipe.strInstructions}</p>
+      <iframe
+        data-testid="video"
+        src={ recipe.strYoutube }
+        title="video"
+        allowFullScreen
+      />
+    </div>
   );
 }
 
